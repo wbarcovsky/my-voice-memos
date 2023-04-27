f16c3daf1d7e7c1265fed9c6cdc11b78e2298569
goodwin <wbarcovsky@yandex.ru>
2023-04-28 03:50:17 +0400

    fix uniqueId test

--- src/utils/uniqueId.test.ts
+++ src/utils/uniqueId.test.ts
@@ -4,7 +4,7 @@ import { uniqueId } from './uniqueId';
 test('test uniqueId utils', () => {
   const val1 = uniqueId();
   const val2 = uniqueId();
-  expect(val1).toMatch(/[0-9a-z]+/);
-  expect(val2).toMatch(/[0-9a-z]+/);
+  expect(val1).toMatch(/[0-9a-z]{6,}/);
+  expect(val2).toMatch(/[0-9a-z]{6,}/);
   expect(val1 !== val2).toBeTruthy();
 });
